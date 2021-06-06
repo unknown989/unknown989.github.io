eb0778adc2ed6de22a57e81b8cff58b3235b1e0d
unknown989 <unknown989@protonmail.com>
2021-06-06 20:05:49 +0000

    Finished contacts and projects pages

--- js/script.js
+++ js/script.js
@@ -39,6 +39,45 @@ function goto(elem,div = null){
 function listrepos(user="unknown989"){
 	var api_url_call = "https://api.github.com/users/"+user+"/repos"
 	 fetch(api_url_call).then((r)=>{
-	 	return r.text().then((text)=>{document.getElementById("pp").innerHTML = text;});
+	 	return r.text().then((text)=>{parserepos(text)});
 	 })
+}
+function parserepos(text){
+	text = JSON.parse(text);
+	var projsdiv = document.getElementById("projs-main")
+	var projslist = document.getElementById("projs-list")
+	text.forEach((item,index)=>{
+		if(item.language !== null){
+
+		var projcard = document.createElement("div");
+		var projcardname = document.createElement("h2");
+		var projcarddesc = document.createElement("div");
+		var projcarddesctext = document.createElement("p");
+		var projcardlang = document.createElement("p");
+		var projcardurl = document.createElement("a");
+		var projcardurlbtn = document.createElement("button");
+		// Non div elements
+		projcardname.innerHTML = item.full_name;
+		projcarddesctext.innerHTML = item.description;
+		projcardlang.innerHTML = "Language :"+item.language;
+		projcardurlbtn.innerHTML = "Go";
+		// Setting attributes
+		projcard.setAttribute("id","proj-card");
+		projcardname.setAttribute("id","proj-card-name");
+		projcarddesc.setAttribute("id","proj-card-desc");
+		projcardlang.setAttribute("id","proj-card-lang");
+		projcardurl.setAttribute("href",item.html_url);
+		projcardurl.setAttribute("target","_blank");
+		projcardurlbtn.setAttribute("class","btn-normal");
+		// Appending elements
+		projcard.appendChild(projcardname);
+		projcarddesc.appendChild(projcarddesctext);
+		projcard.appendChild(projcarddesc);
+		projcard.appendChild(projcardlang);
+		projcardurl.appendChild(projcardurlbtn);
+		projcard.appendChild(projcardurl);
+		projslist.appendChild(projcard);
+		projsdiv.appendChild(projslist);
+		}
+		})
 }
